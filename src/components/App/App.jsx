35959ec7c1e22f1b4d60310c63b00772ed642d43
Shnurkova <Annshnur@gmail.com>
2024-04-29 18:48:54 +0300

    init project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Page title</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae qui, maxime maiores tenetur nihil est ratione recusandae nostrum pariatur illo dolorum magnam non autem natus incidunt quisquam, ullam animi totam?</p>
+    </div>
+  );
+}
